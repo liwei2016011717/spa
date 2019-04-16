ace076176038b3c017d481f0fec6e6487b1932c0
liwei2016011717 <2335644296@qq.com>
2019-04-16 11:10:34 +0800

    进度条

---
+++ 05-progress/main.js
@@ -0,0 +1,18 @@
+$(function(){
+  var $progress=$('progress'),
+      timer=0,
+      i=0;
+  $('#start').click(function(){
+    if(timer!==0) return;
+    timer=window.setInterval(function(){
+      $progress.attr('value',i++);
+    },100);
+  });
+  $('#pause').click(function() {
+    window.clearInterval(timer);
+    timer = 0;
+  });
+  $('#reset').click(function() {
+    $progress.attr('value', i = 0);
+  });
+});
